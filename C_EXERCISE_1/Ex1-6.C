dee16fd200fc2c4aae6473f59a56ac840a7e46d5
Anfal <Egamer8544@gmail.com>
2024-12-04 09:24:55 -0500

    finishe the frist one

---
+++ C_EXERCISE_1/Ex1-6.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define EOF -1
+
+main(){
+    int c, nl, blank, tab;
+    nl = 0;
+    blank = 0;
+    tab = 0;
+    while ( (c = getchar()) != EOF){
+        if (c == '\n'){
+            ++nl;
+        }
+        if (c == '\t'){
+            ++tab;
+        }
+        if (c == ' '){
+            ++blank;
+        }
+        printf("New Lines: %d\n", nl);
+        printf("Tabs: %d\n", tab);
+        printf("Blanks: %d\n", blank);
+        
+    }
+}
