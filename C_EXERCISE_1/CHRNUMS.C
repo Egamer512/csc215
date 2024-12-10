8462128b3a22dd51438b9158732b4d5171d5029f
Anfal <Egamer8544@gmail.com>
2024-12-09 21:21:49 -0500

    working on it

--- C_EXERCISE_1/CHRNUMS.C
+++ C_EXERCISE_1/CHRNUMS.C
@@ -6,16 +6,29 @@ main(argc, argv)
     char* argv[];
 {
     FILE *infp;
-    int count, len;
+    int c, len;
+    c = len = 0;
     char words[MAXLINE];
 
     if (argc != 2){
-        printf("Usage: <infile>\n");
+        printf("Usage: CHRNUMS <infile>\n");
         exit();
     }
     if((infp = fopen(argv[1], "r")) == NULL){
         printf("Can't open %s\n", argv[1]);
         exit();
+    }else{
+        infp = fopen(argv[1], "r")
+        while ((c = fgetc(infp)) != EOF) {
+            if (c == "\n"){
+                words[len] = '\0'; /* If it's a new line, it will make that position to nUll*/
+                printf("%d: %s\n", len, words);
+                len = 0; 
+                continue;
+            }
+            
+        }
     }
+    
 
 }
