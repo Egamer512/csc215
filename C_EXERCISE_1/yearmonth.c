b1eb14a7b5fb4cde34fab311ce2fc01dc3e40624
Anfal <Egamer8544@gmail.com>
2024-12-10 09:19:16 -0500

    finished one func

--- C_EXERCISE_1/yearmonth.c
+++ C_EXERCISE_1/yearmonth.c
@@ -2,3 +2,15 @@ static int days[2][3] ={
     {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
     {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 };
+
+day_of_year(year, month, day)
+int year, month, day;
+{
+    int i, leap;
+
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    for(i = 1; i < month; i++){
+        day += days[leap][i];
+    }
+    return day;
+}
